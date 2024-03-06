efa9f76931673a3d0d3ac8a89797741bfc1f8039
Eloe321 <masterthemanok@gmail.com>
2024-03-07 07:56:39 +0800

    LATEST

--- html/headerOut.php
+++ html/headerOut.php
@@ -1,28 +1,22 @@
 
 <link rel='stylesheet' href='styles.css'>
-<header class="p-3">
+
     <div class="container">
+        <div class="rowHead">
+        <img alt = "Home" style="left: 5%; position: absolute; width: 180px; height: 120px"  src="https://images.unsplash.com/flagged/photo-1555685460-1d9cf532761b?q=80&w=1973&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
+        <ul style="display: flex; flex-direction: row; list-style-type: none" >
+            <li> <a href="#" >Book</a></li>
+            <li><a href = "#">Travel Info</a></li>
+            <li><a href = "#">Explore</a></li>
+            <li><a href = "#">About</a></li>
 
-        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start" >
-            <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-                <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
-            </a>
-
-            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                <li><a href="#" class="nav-link px-2 text-secondary"> Home</a></li>
-                <li><a href="#" class="nav-link px-2 text-white">Posts</a></li>
-            </ul>
-
-
-            <div class="text-end">
-                <p id = "DisplayUser"></p>
-                <button type="button" class = "btn btn-outline-light me-2" id="logout">
-                    Log Out
-                </button>
-            </div>
+        </ul>
+            <button type="button" class="btn btn-light" id="OpenLog">Log In</button>
+            <button type="button" class="btn btn-dark" id="OpenReg">Sign Up</button>
         </div>
+
     </div>
-</header>
+
 
 
 
--- html/script.js
+++ html/script.js
@@ -2,8 +2,14 @@ $(document).ready(function (){
     $('#file1').show();
     $('#file2').hide();
 
-
-
+    window.addEventListener('scroll', function() {
+        let header = document.querySelector('.headOut');
+        if (window.scrollY > 200) { // Adjust 100 to your desired scroll position
+            header.classList.add('scrolled');
+        } else {
+            header.classList.remove('scrolled');
+        }
+    });
 
 
 });
@@ -24,3 +30,6 @@ $("#OpenReg").click(function(){
     window.location.href = "registerPage.php";
 
 });
+
+
+
